fe4201bce30dfcb275b0c4742d5c02ca32a86b34
Konstantin Voykov <koykov0@gmail.com>
2024-02-29 19:59:27 +0200

    gobench: options naming issues.

--- gobench/gobench.sh
+++ gobench/gobench.sh
@@ -10,13 +10,13 @@ else
 fi
 
 count=""
-time_=""
+duration=""
 timeout=""
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -c|--count) count="$2"; shift;;
-        -t|--time) time_="$2"; shift;;
-        -o|--timeout) timeout="$2"; shift;;
+        -d|--duration) duration="$2"; shift;;
+        -t|--timeout) timeout="$2"; shift;;
         *) arg="$1"; shift;;
     esac;
     shift;
@@ -24,15 +24,15 @@ done
 if [ -z "$count" ]; then
     count=1
 fi
-if [ -z "$time_" ]; then
-    time_="1s"
+if [ -z "$duration" ]; then
+    duration="1s"
 fi
 if [ -z "$timeout" ]; then
     timeout="10m"
 fi
 
 case $type in
-    cpu) go test -o /tmp/cpuprofile.test -bench=$bench -benchmem -count=$count -benchtime=$time_ -timeout=$timeout -cpuprofile /tmp/cpuprofile.out ; go tool pprof /tmp/cpuprofile.out ;;
-    mem) go test -o /tmp/memprofile.test -bench=$bench -benchmem -count=$count -benchtime=$time_ -timeout=$timeout -memprofile /tmp/memprofile.out ; go tool pprof /tmp/memprofile.out ;;
+    cpu) go test -o /tmp/cpuprofile.test -bench=$bench -benchmem -count=$count -benchtime=$duration -timeout=$timeout -cpuprofile /tmp/cpuprofile.out ; go tool pprof /tmp/cpuprofile.out ;;
+    mem) go test -o /tmp/memprofile.test -bench=$bench -benchmem -count=$count -benchtime=$duration -timeout=$timeout -memprofile /tmp/memprofile.out ; go tool pprof /tmp/memprofile.out ;;
     *) echo "unknown type: $type" ;;
 esac
